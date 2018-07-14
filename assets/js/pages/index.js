7e662c222ce1deea0fbb5fdd2b5a2b8e22a81279
pfabio <ptrfbn@zergling.hu>
2018-07-14 22:34:36 +0200

    js text processing throttle

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,5 +1,30 @@
 var words = [];
 
-$(function () {
+var crystallizeTimeout;
+var throttle = 500;
 
+var crystallizer = function (rawText) {
+    console.log('modified!');
+    console.log(rawText);
+}
+
+var crystallizerThrottle = function (rawText) {
+    if (!crystallizeTimeout) {
+        crystallizeTimeout = setTimeout(function () {
+            crystallizer(rawText);
+            crystallizeTimeout = null;
+        }, throttle);
+    }
+}
+
+$(function () {
+    $('#raw-text').on('DOMSubtreeModified', function () {
+        var rawText = $(this).html();
+        if (['.', '!', '?', ','].indexOf(rawText.slice(-1)) !== -1
+            || ['&nbsp;'].indexOf(rawText.slice(-6)) !== -1) {
+            crystallizer(rawText);
+        } else {
+            crystallizerThrottle(rawText);
+        }
+    });
 });
--- template-parts/index.php
+++ template-parts/index.php
@@ -2,7 +2,7 @@
     <p class="lead">Type text into the box to see the gender of the nouns</p>
     <div class="row">
         <div class="col-md-10">
-            <div id="raw-text" contenteditable="true" hidefocus="true"></div>
+            <div id="raw-text" contenteditable="true"></div>
             <div id="crystallized-text"></div>
         </div>
     </div>
